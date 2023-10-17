75b72087b43937fc0af711046a418fda148af54c
hyroky867 <handm755@gmail.com>
2023-10-17 23:24:35 +0900

    section: 3-19

--- src/Part3/calculation.ts
+++ src/Part3/calculation.ts
@@ -1,10 +1,15 @@
 const MIN = 0;
 const MAX = 100;
 
-const add = (a: number, b: number): number => {
-  if (a < MIN || a > MAX) {
-    throw new Error(`入力値は${MIN}~${MAX}の間で入力してください`);
+const checkRange = (value: number): void => {
+  if (value < MIN || value > MAX) {
+    throw new RangeError(`入力値は${MIN}~${MAX}の間で入力してください`);
   }
+};
+
+const add = (a: number, b: number): number => {
+  checkRange(a);
+  checkRange(b);
 
   const sum = a + b;
   if (sum > MAX) {

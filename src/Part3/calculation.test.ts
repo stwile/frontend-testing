2e5774fa0f761d30d0b27b8bee19e49c11587f5c
hyroky867 <handm755@gmail.com>
2023-10-17 21:34:19 +0900

    section: 3-9

--- src/Part3/calculation.test.ts
+++ src/Part3/calculation.test.ts
@@ -2,12 +2,21 @@ import { it, expect, describe } from 'vitest';
 
 import { add } from './calculation';
 
-describe('add', () => {
-  it('1 + 1 は 2', () => {
-    expect(add(1, 1)).toBe(2);
-  });
+describe('四則演算', () => {
+  describe('add', () => {
+    it('1 + 1 は 2', () => {
+      expect(add(1, 1)).toBe(2);
+    });
 
-  it('add: 1 + 2 は 3', () => {
-    expect(add(1, 2)).toBe(3);
+    it('add: 1 + 2 は 3', () => {
+      expect(add(1, 2)).toBe(3);
+    });
+
+    it('返り値は、第一引数と第二引数の「和」である', () => {
+      expect(add(50, 50)).toBe(100);
+    });
+    it(`合計の上限は、'100' である`, () => {
+      expect(add(70, 80)).toBe(100);
+    });
   });
 });
--- src/Part3/calculation.ts
+++ src/Part3/calculation.ts
@@ -1,3 +1,10 @@
-const add = (a: number, b: number): number => a + b;
+const add = (a: number, b: number): number => {
+  const sum = a + b;
+  if (sum > 100) {
+    return 100;
+  }
+
+  return sum;
+};
 
 export { add };

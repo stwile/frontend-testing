9d9443a676770305c2c62d5b1b6037d8dbc15e23
hyroky867 <handm755@gmail.com>
2023-10-17 23:33:34 +0900

    section: 3-39

--- src/Part3/promise.test.ts
+++ src/Part3/promise.test.ts
@@ -2,5 +2,6 @@ import { it, expect } from 'vitest';
 
 import { wait } from './promise';
 
-it('指定時間を待つと、経過時間をもってresolveされる', () =>
-  expect(wait(50)).resolves.toBe(50));
+it('指定時間を待つと、経過時間をもってresolveされる', async () => {
+  await expect(wait(50)).resolves.toBe(50);
+});
